c336c9e05cb58569b471892181f6a67e104e35ed
LyDuy284 <lyhieuduy9190@gmail.com>
2024-03-14 10:30:47 +0700

    Add auction

---
+++ src/configs/navigation-icon.config.tsx
@@ -0,0 +1,18 @@
+import { BsFileEarmarkPostFill } from 'react-icons/bs'
+import { FaUsers } from 'react-icons/fa'
+import { HiOutlineCurrencyDollar, HiOutlineViewGridAdd } from 'react-icons/hi'
+import { RxDashboard } from 'react-icons/rx'
+import { BsAwardFill } from 'react-icons/bs'
+
+export type NavigationIcons = Record<string, JSX.Element>
+
+const navigationIcon: NavigationIcons = {
+  apps: <HiOutlineViewGridAdd />,
+  crypto: <HiOutlineCurrencyDollar />,
+  dashboard: <RxDashboard />,
+  user: <FaUsers />,
+  post: <BsFileEarmarkPostFill />,
+  aution: <BsAwardFill />,
+}
+
+export default navigationIcon
